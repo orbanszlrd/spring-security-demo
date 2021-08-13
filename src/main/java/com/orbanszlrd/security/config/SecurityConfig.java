0ec6ad895b7679bbd7d261f91dfb1a17faab978b
Orban Szilard <orbanszlrd@gmail.com>
2021-08-13 15:43:39 +0200

    Add jwt dependencies

--- src/main/java/com/orbanszlrd/security/config/SecurityConfig.java
+++ src/main/java/com/orbanszlrd/security/config/SecurityConfig.java
@@ -6,25 +6,18 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    PasswordEncoder  passwordEncoder;
+    UserDetailsService userDetailService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.ldapAuthentication()
-        .userDnPatterns("uid={0},ou=people")
-        .groupSearchBase("ou=groups")
-        .contextSource()
-        .url("ldap://localhost:8389/dc=springframework,dc=org")
-        .and()
-        .passwordCompare()
-        .passwordEncoder(passwordEncoder)
-        .passwordAttribute("userPassword");
+        auth.userDetailsService(userDetailService);
     }
 
     @Bean
@@ -34,9 +27,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and().csrf().disable()
+                .headers().frameOptions().sameOrigin().and()
                 .authorizeRequests()
-                .anyRequest().fullyAuthenticated()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("OWNER", "ADMIN")
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/**").permitAll()
                 .and().formLogin()
         ;
 
